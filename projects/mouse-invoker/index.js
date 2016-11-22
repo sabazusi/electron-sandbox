10879b21ba8bcba24306fdf8faf4d9458dbd9658
sabazusi <kohei_tsurubo@dwango.co.jp>
2016-11-22 14:12:42 +0900

    Trace current mouse point

--- projects/mouse-invoker/index.js
+++ projects/mouse-invoker/index.js
@@ -1,5 +1,8 @@
-import {app} from 'electron';
+import {app, screen} from 'electron';
 
 app.on('ready', () => {
   console.log('started');
+  setInterval(() => {
+    console.log(screen.getCursorScreenPoint());
+  }, 10);
 });
