079ee80bec7ddf3047bb7e6bf80f79a046ade210
sabazusi <kouhei.live@gmail.com>
2016-11-24 23:58:41 +0900

    Detect whether cursor is top of screen

--- projects/mouse-invoker/index.js
+++ projects/mouse-invoker/index.js
@@ -3,6 +3,8 @@ import {app, screen} from 'electron';
 app.on('ready', () => {
   console.log('started');
   setInterval(() => {
-    console.log(screen.getCursorScreenPoint());
+    const pos = screen.getCursorScreenPoint();
+    console.log(screen.getAllDisplays());
+    if (pos.y === 0) console.log('On TOP');
   }, 10);
 });
