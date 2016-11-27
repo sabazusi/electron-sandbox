761de6c7d7880c7f61b2f75a8353b7398fd7475b
sabazusi <kouhei.live@gmail.com>
2016-11-27 22:39:04 +0900

    Switch visibility with mouse position

--- projects/mouse-invoker/index.js
+++ projects/mouse-invoker/index.js
@@ -1,10 +1,20 @@
-import {app, screen} from 'electron';
+import {app, BrowserWindow, screen} from 'electron';
 
 app.on('ready', () => {
   console.log('started');
+  const win = new BrowserWindow({
+    width: 300,
+    height: 300,
+    show: false
+  });
+
   setInterval(() => {
     const pos = screen.getCursorScreenPoint();
     console.log(screen.getAllDisplays());
-    if (pos.y === 0) console.log('On TOP');
+    if (pos.y < 10) {
+      win.show();
+    } else {
+      win.hide();
+    }
   }, 10);
 });
