dd9ee6ef23b09460f7dbee295bdc27749164f6eb
sabazusi <kouhei.live@gmail.com>
2017-03-26 10:12:55 +0900

    Create 10 window

--- projects/new-window-position/index.js
+++ projects/new-window-position/index.js
@@ -1,21 +1,25 @@
 import {app, BrowserWindow, screen, globalShortcut} from 'electron';
 import path from 'path';
 
+const windows = [];
+const MAX_WINDOW_NUM = 10;
 app.on('ready', () => {
-  const win = new BrowserWindow({
-    width: 200,
-    height: 200,
-    x: 0,
-    y: 0,
-    show: false,
-    resizable: false,
-    movable: true,
-    alwaysOnTop: true,
-    frame: false
-  });
+  const createWindow = () => {
+    if (windows.length < MAX_WINDOW_NUM) {
+      const win = new BrowserWindow({
+        width: 200,
+        height: 200,
+        x: windows.length * 100,
+        y: windows.length * 50,
+        show: true,
+        resizable: false,
+        movable: true,
+        alwaysOnTop: true,
+        frame: false
+      });
+      windows.push(win);
+    }
+  };
 
-  win.loadURL(`file://${path.resolve(__dirname, '../')}/window.html`);
-  setTimeout(() => {
-    win.show();
-  }, 500);
+  setInterval(() => createWindow(), 3000);
 });
